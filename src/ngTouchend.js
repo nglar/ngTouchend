1712848bab6292952dbc7b8fc1a5fbea72381313
Mark Topper <mail@webman.io>
2015-09-18 14:28:42 +0200

    Added $event to $scope.

--- src/ngTouchend.js
+++ src/ngTouchend.js
@@ -1,15 +1,1 @@
-"use strict";
-
-angular.module("ngTouchend", []).directive("ngTouchend", function () {
-  return {
-    controller: function ($scope, $element, $attrs) {
-      $element.bind('touchend', onTouchEnd);
-      
-      function onTouchEnd(event) {
-        var method = $element.attr('ng-touchend');
-        $scope.$event = event;
-        $scope.$apply(method);
-      };
-    }
-  };
-});
+"use strict";angular.module("ngTouchend",[]).directive("ngTouchend",function(){return{controller:function(n,t){function e(e){var u=t.attr("ng-touchend");n.$event=e,n.$apply(u)}t.bind("touchend",e)}}});
